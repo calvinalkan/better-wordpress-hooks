f7c788259f8a7937396a51aade0c3eebab128114
Calvin Alkan <calvin@snicco.de>
2021-05-20 12:50:56 -0500

    refactor test suite and run test that test the inbuilt test case in a separate process on circleci (#16)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,9 @@
     declare(strict_types = 1);
 
 
+    $ds = DIRECTORY_SEPARATOR;
+
+
     $root_dir = getenv('ROOT_DIR');
 
     if ( ! $root_dir || ! is_dir($root_dir) ) {
@@ -14,9 +17,12 @@
 
     if ( $root_dir ) {
 
-        define('ROOT_DIR', rtrim($root_dir , DIRECTORY_SEPARATOR));
-        define('TESTS_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'tests');
+
+        define('ROOT_DIR', rtrim($root_dir , $ds));
+        define('TESTS_DIR', ROOT_DIR . $ds . 'tests');
+        define('VENDOR_DIR', ROOT_DIR . $ds . 'vendor');
+        define('PLUGIN_PHP', ROOT_DIR . $ds . 'vendor' . $ds . 'calvinalkan' . $ds . 'wordpress-hook-api-clone'. $ds . 'plugin.php');
 
     }
 
-    require_once ROOT_DIR . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+    require_once ROOT_DIR . $ds . 'vendor' . $ds . 'autoload.php';
